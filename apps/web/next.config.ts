18d11539120a593662446173fc775987dfe583f4
Jahzeal <jahzealibeh3@gmail.com>
2026-07-13 14:42:06 +0100

    csp

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval' https://auth.privy.io; frame-src 'self' https://auth.privy.io;",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' https://auth.privy.io; frame-src 'self' https://auth.privy.io;",
           },
         ],
       },
